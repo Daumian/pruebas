41408e0ce988ca7a78d21b7b2ce6fb132ac8f8ec
Daumian Ruiz <daumianruiz@gmail.com>
2020-01-07 15:39:05 -0300

    iniciamos

---
+++ 2x2 estadisticas.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+double A=0;
+
+double PorC=100;
+double Divisor=10000;
+
+int main(){
+
+	cout<<"ingrese la posibilidad de que algo suceda (en %)"<<endl;
+		cout<<"Posibilidad de que pase :   ";cin>>A; 
+			cout<<endl;
+	
+			
+double a=PorC-A;
+			cout<<endl;
+			
+	
+	cout<<"si hubieran 2 personas";cout<<endl;
+	
+	cout<<"posibilidades de que a ambos les pase:     ";cout<<(PorC/Divisor)*(A*A);cout<<" %  ";cout<<endl;
+	cout<<"posibilidades de que solo a uno le pase:   ";cout<<(PorC/Divisor)*(a*A)+(PorC/Divisor)*(A*a);cout<<" %  ";cout<<endl;
+	cout<<"posibilidades de que a ninguno le pase:    ";cout<<(PorC/Divisor)*(a*a);cout<<" %  ";cout<<endl;
+	
+	
+
+	
+	
+
+		cout<<endl;
+
+
+
+
+
+return 0;
+}
